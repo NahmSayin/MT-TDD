42c5638f610007ee617bf0b3f6a806eb45570b15
nahomalem <nahom.alem@education.gov.uk>
2026-09-01 17:35:48 +0100

    Green: changed float to decimal to avoid floating point error

--- src/StringCalculator/StringCalculator.cs
+++ src/StringCalculator/StringCalculator.cs
@@ -11,7 +11,7 @@ public static class StringCalculator
 
         return input
             .Split(',')
-            .Select(float.Parse)
+            .Select(decimal.Parse)
             .ToArray()
             .Sum()
             .ToString();
